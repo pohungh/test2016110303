16009887b9fe613bd737d71239b84a13e687aa25
pohung <kof96302@yahoo.com.tw>
2016-11-03 19:49:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/test2016110303/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.yvtc.test2016110303;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String[] fruits = {"蘋果","香蕉","鳳梨","芭樂","檸檬"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lv = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
+                                android.R.layout.simple_list_item_1,
+                                fruits);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,fruits[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
